01b7318defe3490ac4425b4e7a089f9b127cacef
Palash Ray <paawak@gmail.com>
2017-12-03 20:19:42 +0530

    added navbar

--- fullstack-dev/trading-app-front-end/src/app/app.module.ts
+++ fullstack-dev/trading-app-front-end/src/app/app.module.ts
@@ -1,16 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { AlertModule } from 'ngx-bootstrap';
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AlertModule.forRoot()
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
