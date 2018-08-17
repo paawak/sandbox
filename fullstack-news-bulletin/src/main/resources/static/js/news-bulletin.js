eb8c49983d1b0d8d1a688cbc0b84a0406f696f7a
Palash Ray <paawak@gmail.com>
2018-08-17 15:17:24 +0530

    using jquery append, proper err handling

--- fullstack-news-bulletin/src/main/resources/static/js/news-bulletin.js
+++ fullstack-news-bulletin/src/main/resources/static/js/news-bulletin.js
@@ -16,17 +16,15 @@ function loadNewsItems(newsItemsArray) {
 }
 
 function addNews(newsItem) {
-    var tableDiv = document.getElementById("tableDiv");
     var newDivRow = '<div class="row">' 
-	+ '<div class="col-6"><a href=' + newsItem.url + '>' + newsItem.headline + '</a></div>'
-	+ '<div class="col"><a href=http://' + newsItem.publisherUrl + '>' + newsItem.publisherName + '</a></div>'
+	+ '<div class="col-6"><a href="' + newsItem.url + '">' + newsItem.headline + '</a></div>'
+	+ '<div class="col"><a href="http://' + newsItem.publisherUrl + '">' + newsItem.publisherName + '</a></div>'
 	+ '<div class="col">' + newsItem.publishedTime + '</div>'
 	+ '</div>'; 
-    tableDiv.innerHTML += newDivRow;
+    $("#tableDiv").append(newDivRow);
 }
 
 function showAlert() {
 	var alertMessage = '<div class="alert alert-warning alert-dismissible fade show"' +'		role="alert">' +'		<strong>Sorry!</strong> Could not load news items.' +'		<button type="button" class="close" data-dismiss="alert"' +'			aria-label="Close">' +'			<span aria-hidden="true">&times;</span>' +'		</button>' +'	</div>';
-	var divAlert = document.getElementById("divAlert");
-	divAlert.innerHTML += alertMessage;
+	$("#divAlert").append(alertMessage);
 }
