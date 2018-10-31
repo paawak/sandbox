0ec9ccf96e70b206fdde6cb94ea0ea022caf0255
Palash Ray <paawak@gmail.com>
2018-10-31 13:27:11 +0530

    logging message

--- spell-checker/src/main/java/com/swayam/spellchecker/SpellChecker.java
+++ spell-checker/src/main/java/com/swayam/spellchecker/SpellChecker.java
@@ -11,22 +11,22 @@ public class SpellChecker {
     private static final Logger LOGGER = LoggerFactory.getLogger(SpellChecker.class);
 
     public static void main(String[] args) {
-        Collection<String> allWords = new DictionaryCrawler().crawl();
-        LOGGER.trace("allWords: {}", allWords);
+	Collection<String> allWords = new DictionaryCrawler().crawl();
+	LOGGER.trace("allWords: {}", allWords);
 
-        WordAnalyser wordAnalyser = new WordAnalyser(allWords);
+	WordAnalyser wordAnalyser = new WordAnalyser(allWords);
 
-        LOGGER.info("Initialization complete, ready to accept input");
-        try (Scanner scanner = new Scanner(System.in);) {
-            while (scanner.hasNextLine()) {
-                String word = scanner.nextLine().toLowerCase();
-                if (!DictionaryCrawler.WORD.matcher(word).matches()) {
-                    LOGGER.warn("Wrong input: expecting only words");
-                    continue;
-                }
-                System.out.println(wordAnalyser.doSpellCheck(word));
-            }
-        }
+	LOGGER.info("Initialization complete, ready to accept input");
+	try (Scanner scanner = new Scanner(System.in);) {
+	    while (scanner.hasNextLine()) {
+		String word = scanner.nextLine().toLowerCase();
+		if (!DictionaryCrawler.WORD.matcher(word).matches()) {
+		    LOGGER.warn("Wrong input: expecting only letters from A to Z in upper case or lower case");
+		    continue;
+		}
+		System.out.println(wordAnalyser.doSpellCheck(word));
+	    }
+	}
 
     }
 
