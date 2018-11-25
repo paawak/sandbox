b530deb30d3130dfce8d8e70caeed2b6ec463183
Palash Ray <paawak@gmail.com>
2018-11-25 23:38:19 +0530

    define node

--- clojure/recursion/src/recursion/binarytree.clj
+++ clojure/recursion/src/recursion/binarytree.clj
@@ -4,20 +4,29 @@
 
 (defrecord Node[value left right])
 
+(def root nil)
 
-(defn add [n]
-  (
+(defn addNode [n]
     (println "Trying to add " n " into the Tree")
     (def newNode (->Node n nil nil))
-    )
+    (println "11111111")
+    (if (nil? root)
+      (do (
+            println "5555555"
+            (def root newNode)
+            println "666666666 " root
+            )
+        )
+      )
+    (println "22222222 " root)
+    (println "The root of the tree is: " root)
   )
 
 (defn -main
   []
-  (
-    (add 4)
-    (println "Hello!")
-    )
-
+    (println "3333333")
+    (addNode 4)
+    (println "444444444")
+    (println "The root of the tree is: " root)
   )
 
