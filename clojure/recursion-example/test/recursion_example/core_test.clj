625e42a43b0f7a6b1554d9dda8283e29cf6c5320
Palash Ray <paawak@gmail.com>
2018-11-25 10:31:09 +0530

    blank project

---
+++ clojure/recursion-example/test/recursion_example/core_test.clj
@@ -0,0 +1,7 @@
+(ns recursion-example.core-test
+  (:require [clojure.test :refer :all]
+            [recursion-example.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
