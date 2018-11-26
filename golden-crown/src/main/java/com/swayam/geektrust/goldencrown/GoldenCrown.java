612da5c4a86f3de623a0b7f833c323e55d808631
Palash Ray <paawak@gmail.com>
2018-11-26 15:49:29 +0530

    1st cut

---
+++ golden-crown/src/main/java/com/swayam/geektrust/goldencrown/GoldenCrown.java
@@ -0,0 +1,17 @@
+package com.swayam.geektrust.goldencrown;
+
+import java.util.Scanner;
+
+public class GoldenCrown {
+
+    public static void main(String[] args) {
+
+        try (Scanner scanner = new Scanner(System.in);) {
+            while (scanner.hasNextLine()) {
+
+            }
+        }
+
+    }
+
+}
