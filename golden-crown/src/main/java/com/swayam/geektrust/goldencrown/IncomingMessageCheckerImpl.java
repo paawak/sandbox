dc83ee004963ab59d403c17b47b0505792c19fa7
Palash Ray <paawak@gmail.com>
2018-11-26 17:27:56 +0530

    a

--- golden-crown/src/main/java/com/swayam/geektrust/goldencrown/IncomingMessageCheckerImpl.java
+++ golden-crown/src/main/java/com/swayam/geektrust/goldencrown/IncomingMessageCheckerImpl.java
@@ -1,11 +1,26 @@
 package com.swayam.geektrust.goldencrown;
 
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class IncomingMessageCheckerImpl implements IncomingMessageChecker {
 
     @Override
     public boolean messageAccepted(Kingdom kingdom, String rawMessage) {
-        // TODO Auto-generated method stub
+
         return false;
+    }
+
+    private Map<Character, Integer> getCharacterFrequency(String word) {
+
+        return word.chars().mapToObj((int n) -> (char) n).collect(Collectors.toMap((Character c) -> {
+            return Character.toLowerCase(c);
+        }, (Character c) -> {
+            return 1;
+        }, (Integer val1, Integer val2) -> {
+            return val1 + val2;
+        }));
+
     }
 
 }
