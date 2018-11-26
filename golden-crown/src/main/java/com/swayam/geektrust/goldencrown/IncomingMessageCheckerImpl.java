61832c81b65df34a4ca589d60c3cb6afcfe3de3b
Palash Ray <paawak@gmail.com>
2018-11-26 19:54:57 +0530

    c

--- golden-crown/src/main/java/com/swayam/geektrust/goldencrown/IncomingMessageCheckerImpl.java
+++ golden-crown/src/main/java/com/swayam/geektrust/goldencrown/IncomingMessageCheckerImpl.java
@@ -1,6 +1,8 @@
 package com.swayam.geektrust.goldencrown;
 
+import java.util.Collections;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.stream.Collectors;
 
 public class IncomingMessageCheckerImpl implements IncomingMessageChecker {
@@ -8,13 +10,22 @@ public class IncomingMessageCheckerImpl implements IncomingMessageChecker {
     @Override
     public boolean messageAccepted(Kingdom kingdom, String rawMessage) {
 
-        return false;
+        Map<Character, Integer> animalEmblemWordFrequency = getCharacterFrequency(kingdom.getAnimalEmblem());
+        Map<Character, Integer> rawMessageWordFrequency = getCharacterFrequency(rawMessage);
+
+        long noMatchCharacterCount = animalEmblemWordFrequency.entrySet().stream().filter((Entry<Character, Integer> entry) -> {
+            return !rawMessageWordFrequency.containsKey(entry.getKey()) || (rawMessageWordFrequency.get(entry.getKey()) != entry.getValue());
+        }).count();
+
+        return noMatchCharacterCount == 0;
     }
 
     Map<Character, Integer> getCharacterFrequency(String word) {
 
-        return word.chars().mapToObj((int character) -> (char) character)
+        Map<Character, Integer> characterFrequency = word.chars().mapToObj((int character) -> (char) character)
                 .collect(Collectors.toMap(Character::toLowerCase, character -> 1, (oldValue, newValue) -> oldValue + newValue));
+
+        return Collections.unmodifiableMap(characterFrequency);
 
     }
 
