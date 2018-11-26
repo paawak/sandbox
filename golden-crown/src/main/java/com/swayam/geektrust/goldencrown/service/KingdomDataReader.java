7050412552246ead2818b09f541eb0ace2d073b3
Palash Ray <paawak@gmail.com>
2018-11-26 22:13:29 +0530

    a

--- golden-crown/src/main/java/com/swayam/geektrust/goldencrown/service/KingdomDataReader.java
+++ golden-crown/src/main/java/com/swayam/geektrust/goldencrown/service/KingdomDataReader.java
@@ -5,9 +5,9 @@ import java.io.IOException;
 import java.io.Reader;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
+import java.util.stream.Collectors;
 
 import com.swayam.geektrust.goldencrown.model.Kingdom;
 import com.swayam.geektrust.goldencrown.model.KingdomImpl;
@@ -16,37 +16,41 @@ import com.swayam.geektrust.goldencrown.model.SoutherosKingdom;
 public class KingdomDataReader {
 
     public Map<SoutherosKingdom, Kingdom> readAvailableKingdoms(Reader dataReader) {
-        Map<SoutherosKingdom, Kingdom> availableKingdoms = new HashMap<>();
 
         try (BufferedReader br = new BufferedReader(dataReader)) {
 
-            br.lines().filter(line -> line.trim().length() > 0).map(line -> {
-                String[] keyValuePair = line.split("=");
-                if (keyValuePair.length != 2) {
-                    throw new IllegalStateException("Wrong input format: expecting the line to be of the format: <Kingdom Name>=<Kingdom Data>");
-                }
-                SoutherosKingdom southerosKingdom = SoutherosKingdom.valueOf(keyValuePair[0].trim());
-                String kingdomData = keyValuePair[1].trim();
-                String[] kingdomDataParts = kingdomData.split(",");
+            Map<SoutherosKingdom, Kingdom> availableKingdoms = br.lines().filter(line -> line.trim().length() > 0).map(line -> parseDataLine(line))
+                    .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
 
-                Entry<SoutherosKingdom, Kingdom> entry;
-
-                if (kingdomDataParts.length == 1) {
-                    entry = new SimpleEntry<>(southerosKingdom, new KingdomImpl(kingdomData));
-                } else if (kingdomDataParts.length == 2) {
-                    entry = new SimpleEntry<>(southerosKingdom, new KingdomImpl(kingdomDataParts[0].trim(), kingdomDataParts[1].trim()));
-                } else {
-                    throw new UnsupportedOperationException("This <Kingdom Data> format is not yet supported");
-                }
-
-                return entry;
-            });
+            return Collections.unmodifiableMap(availableKingdoms);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-        return Collections.unmodifiableMap(availableKingdoms);
+    }
+
+    /* Visible for testing */
+    Entry<SoutherosKingdom, Kingdom> parseDataLine(String line) {
+        String[] keyValuePair = line.split("=");
+        if (keyValuePair.length != 2) {
+            throw new IllegalStateException("Wrong input format: expecting the line to be of the format: <Kingdom Name>=<Kingdom Data>");
+        }
+        SoutherosKingdom southerosKingdom = SoutherosKingdom.valueOf(keyValuePair[0].trim());
+        String kingdomData = keyValuePair[1].trim();
+        String[] kingdomDataParts = kingdomData.split(",");
+
+        Entry<SoutherosKingdom, Kingdom> entry;
+
+        if (kingdomDataParts.length == 1) {
+            entry = new SimpleEntry<>(southerosKingdom, new KingdomImpl(kingdomData));
+        } else if (kingdomDataParts.length == 2) {
+            entry = new SimpleEntry<>(southerosKingdom, new KingdomImpl(kingdomDataParts[0].trim(), kingdomDataParts[1].trim()));
+        } else {
+            throw new UnsupportedOperationException("This <Kingdom Data> format is not yet supported");
+        }
+
+        return entry;
     }
 
 }
