660cfbddb4b5551177cd3b170dd21c764c2d2df3
Palash Ray <paawak@gmail.com>
2018-11-26 17:49:18 +0530

    a

--- golden-crown/src/test/java/com/swayam/geektrust/goldencrown/IncomingMessageCheckerImplTest.java
+++ golden-crown/src/test/java/com/swayam/geektrust/goldencrown/IncomingMessageCheckerImplTest.java
@@ -1,7 +1,11 @@
 package com.swayam.geektrust.goldencrown;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import org.junit.Test;
 
@@ -41,6 +45,26 @@ public class IncomingMessageCheckerImplTest {
 
         // then
         assertFalse(result);
+    }
+
+    @Test
+    public void testGetCharacterFrequency() {
+        // given
+        Map<Character, Integer> expected = new HashMap<>();
+        expected.put('p', 3);
+        expected.put('a', 3);
+        expected.put('b', 1);
+        expected.put('g', 1);
+        expected.put('2', 1);
+        expected.put('5', 1);
+        expected.put('#', 1);
+        IncomingMessageCheckerImpl testClass = new IncomingMessageCheckerImpl();
+
+        // when
+        Map<Character, Integer> result = testClass.getCharacterFrequency("pAaaPP#bG25");
+
+        // then
+        assertEquals(expected, result);
     }
 
 }
