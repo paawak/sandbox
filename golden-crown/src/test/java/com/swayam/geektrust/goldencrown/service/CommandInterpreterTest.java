3af60b4e9d79ab04a56764ef86449d6e51e1586f
Palash Ray <paawak@gmail.com>
2018-11-27 16:27:38 +0530

    c

--- golden-crown/src/test/java/com/swayam/geektrust/goldencrown/service/CommandInterpreterTest.java
+++ golden-crown/src/test/java/com/swayam/geektrust/goldencrown/service/CommandInterpreterTest.java
@@ -9,12 +9,48 @@ import com.swayam.geektrust.goldencrown.model.Command;
 public class CommandInterpreterTest {
 
     @Test
-    public void testParseCommand_FIND_RULER() {
+    public void testParseCommand_FIND_RULER_mixed_case() {
 	// given
 	CommandInterpreter testClass = new CommandInterpreter();
 
 	// when
 	Command result = testClass.parseCommand("Who is the ruler of Southeros?");
+
+	// then
+	assertEquals(Command.FIND_RULER, result);
+    }
+
+    @Test
+    public void testParseCommand_FIND_RULER_lower_case() {
+	// given
+	CommandInterpreter testClass = new CommandInterpreter();
+
+	// when
+	Command result = testClass.parseCommand("who is the ruler of southeros?");
+
+	// then
+	assertEquals(Command.FIND_RULER, result);
+    }
+
+    @Test
+    public void testParseCommand_FIND_RULER_upper_case() {
+	// given
+	CommandInterpreter testClass = new CommandInterpreter();
+
+	// when
+	Command result = testClass.parseCommand("WHO IS THE RULER OF SOUTHEROS?");
+
+	// then
+	assertEquals(Command.FIND_RULER, result);
+    }
+
+    @Test
+    public void testParseCommand_FIND_RULER_many_spaces() {
+	// given
+	CommandInterpreter testClass = new CommandInterpreter();
+
+	// when
+	Command result = testClass.parseCommand("Who     is    the    ruler    of    Southeros   ?");
 
 	// then
 	assertEquals(Command.FIND_RULER, result);
