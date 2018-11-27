c151f01a46b158c8ee5b8bc950a8cbf575eb553c
Palash Ray <paawak@gmail.com>
2018-11-27 08:00:56 +0530

    use org.mockito.ArgumentMatchers.any

--- golden-crown/src/test/java/com/swayam/geektrust/goldencrown/service/KingdomDataReaderTest.java
+++ golden-crown/src/test/java/com/swayam/geektrust/goldencrown/service/KingdomDataReaderTest.java
@@ -1,7 +1,7 @@
 package com.swayam.geektrust.goldencrown.service;
 
 import static org.junit.Assert.assertEquals;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
