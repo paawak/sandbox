a9bde04e567548f2ca2c5ecd2bf29bb5e942f7c0
Palash Ray <paawak@gmail.com>
2018-11-27 18:47:29 +0530

    a

--- golden-crown/src/test/java/com/swayam/geektrust/goldencrown/service/command/AlliesOfKingFinderCommandTest.java
+++ golden-crown/src/test/java/com/swayam/geektrust/goldencrown/service/command/AlliesOfKingFinderCommandTest.java
@@ -2,11 +2,17 @@ package com.swayam.geektrust.goldencrown.service.command;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Arrays;
+import java.util.HashSet;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
 
+import com.swayam.geektrust.goldencrown.model.Kingdom;
+import com.swayam.geektrust.goldencrown.model.KingdomData;
 import com.swayam.geektrust.goldencrown.service.KingdomService;
 
 public class AlliesOfKingFinderCommandTest {
@@ -24,6 +30,23 @@ public class AlliesOfKingFinderCommandTest {
 
 	// then
 	assertEquals("abcdefg", result);
+    }
+
+    @Test
+    public void testGetKingdom_found_match() {
+	// given
+	KingdomService kingdomService = mock(KingdomService.class);
+
+	when(kingdomService.getKingdomsInSoutheros()).thenReturn(new HashSet<>(Arrays.asList(new KingdomData(Kingdom.AIR, "someAnimal", "ShaN"), new KingdomData(Kingdom.LAND, "someAnimal", "ShaN34"),
+		new KingdomData(Kingdom.WATER, "someAnimal", "67shan"), new KingdomData(Kingdom.ICE, "someAnimal", null))));
+
+	AlliesOfKingFinderCommand testClass = new AlliesOfKingFinderCommand(kingdomService);
+
+	// when
+	Kingdom result = testClass.getKingdom("shan");
+
+	// then
+	assertEquals(Kingdom.AIR, result);
     }
 
     @Test
