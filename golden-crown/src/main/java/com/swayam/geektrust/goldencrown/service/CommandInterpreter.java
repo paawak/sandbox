1787763b3041492e0f6e2109b4d718021bfb95f5
Palash Ray <paawak@gmail.com>
2018-11-27 16:46:37 +0530

    a

--- golden-crown/src/main/java/com/swayam/geektrust/goldencrown/service/CommandInterpreter.java
+++ golden-crown/src/main/java/com/swayam/geektrust/goldencrown/service/CommandInterpreter.java
@@ -1,11 +1,24 @@
 package com.swayam.geektrust.goldencrown.service;
 
+import java.util.regex.Pattern;
+
 import com.swayam.geektrust.goldencrown.model.Command;
 
 public class CommandInterpreter {
 
+    private static final String FIND_RULER_REGEX = "^who\\s+is\\s+the\\s+ruler\\s+of\\s+southeros\\s*\\?$";
+    private static final String FIND_ALLIES_OF_RULER_REGEX = "^allies\\s+of\\w+\\?$";
+
     public Command parseCommand(String rawCommand) {
-	return null;
+	String rawCommandInLowerCase = rawCommand.toLowerCase();
+	if (Pattern.matches(FIND_RULER_REGEX, rawCommandInLowerCase)) {
+	    return Command.FIND_RULER;
+	} else if (Pattern.matches(FIND_ALLIES_OF_RULER_REGEX, rawCommandInLowerCase)) {
+	    return Command.FIND_ALLIES_OF_RULER;
+	} else {
+	    return Command.UNKNOWN;
+	}
+
     }
 
 }
