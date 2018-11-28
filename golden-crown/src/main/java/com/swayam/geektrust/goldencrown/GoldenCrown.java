3ae47fbad5eb821addc3d5c1cd25130c38631f5a
Palash Ray <paawak@gmail.com>
2018-11-28 14:58:52 +0530

    q

--- golden-crown/src/main/java/com/swayam/geektrust/goldencrown/GoldenCrown.java
+++ golden-crown/src/main/java/com/swayam/geektrust/goldencrown/GoldenCrown.java
@@ -2,15 +2,19 @@ package com.swayam.geektrust.goldencrown;
 
 import java.util.Scanner;
 
+import com.swayam.geektrust.goldencrown.dao.KingdomRepository;
+import com.swayam.geektrust.goldencrown.dao.KingdomRepositoryImpl;
 import com.swayam.geektrust.goldencrown.service.CommandInterpreter;
 import com.swayam.geektrust.goldencrown.service.KingdomService;
+import com.swayam.geektrust.goldencrown.service.KingdomServiceImpl;
 import com.swayam.geektrust.goldencrown.service.command.Command;
 
 public class GoldenCrown {
 
     public static void main(String[] args) {
 
-	KingdomService kingdomService = null;
+	KingdomRepository kingdomRepository = new KingdomRepositoryImpl("/goldencrown/data/kingdom_data.properties");
+	KingdomService kingdomService = new KingdomServiceImpl(kingdomRepository);
 	CommandInterpreter commandInterpreter = new CommandInterpreter(kingdomService);
 
 	Command commandToRepeat = null;
