bc2dc21761422e53f39bac1950f0098843078d71
Palash Ray <paawak@gmail.com>
2018-11-29 18:55:15 +0530

    normal recursion, without loop

--- clojure/recursion/src/recursion/binarytree.clj
+++ clojure/recursion/src/recursion/binarytree.clj
@@ -6,51 +6,40 @@
 
 (def root (atom (->Node nil nil nil)))
 
-(defn addNode [n]
-    (println "Trying to add " n " into the Tree")
-    (if (nil? (:value @root))
+(defn addNode [n currentNode]
+    (if (nil? (:value @currentNode))
       (do
-            (println "Assigning the newNode to root")
-            (swap! root assoc :value n)
+            (swap! currentNode assoc :value n)
         )
       (do
-        (println "Root exists")
-        (loop [currentNode root]
           (if (= n (:value @currentNode))
             (println "Ignore silently, as duplicates are not allowed")
             (do
-              (cond (< n (:value @currentNode))
-                (do
-                  (println "Left")
+              (cond (> n (:value @currentNode))
                   (if (nil? (:left @currentNode))
                     (swap! currentNode assoc :left (atom (->Node n nil nil)))
-                    (recur (:left @currentNode))
+                    (addNode n (:left @currentNode))
                     )
-                )
-                (> n (:value @currentNode))
-                (do
-                  (println "Right")
+                (< n (:value @currentNode))
                   (if (nil? (:right @currentNode))
                     (swap! currentNode assoc :right (atom (->Node n nil nil)))
-                    (recur (:right @currentNode))
+                    (addNode n (:right @currentNode))
                     )
-                )
                 )
               )
             )
           
-          )
         )
       )
-    (println "--------------Added a new Node")
   )
 
 (defn printElements
   [node]
-    (println (:value @node))
+    
     (if (:left @node)
       (printElements (:left @node))
       )
+    (print (:value @node) "  ")
     (if (:right @node)
       (printElements (:right @node))
       )    
@@ -58,10 +47,19 @@
 
 (defn -main
   []
-    (addNode 4)
-    (addNode 3)
-    (addNode 6)
-    (addNode 4)
+    (addNode 4 root)
+    (addNode 3 root)
+    (addNode 6 root)
+    (addNode 14 root)
+    (addNode 36 root)
+    (addNode 100 root)
+    (addNode 20 root)
+    (addNode 19 root)
+    (addNode 70 root)
+    (addNode 81 root)
+    (addNode 97 root)
+    (addNode 200 root)
+    (println "Printing nodes of the tree in pre-order")
     (printElements root)
   )
 
