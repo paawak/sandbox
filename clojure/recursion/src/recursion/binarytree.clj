554ae87d29bb65f8f5dc9cf378f5f608a468445d
Palash Ray <paawak@gmail.com>
2018-11-29 19:04:15 +0530

    renmove un-necessary do

--- clojure/recursion/src/recursion/binarytree.clj
+++ clojure/recursion/src/recursion/binarytree.clj
@@ -7,42 +7,26 @@
 (def root (atom (->Node nil nil nil)))
 
 (defn addNode [n]
-;    (println "Trying to add " n " into the Tree")
     (if (nil? (:value @root))
-      (do
-;            (println "Assigning the newNode to root")
-            (swap! root assoc :value n)
-        )
-      (do
-        (println "Root exists")
+      (swap! root assoc :value n)
         (loop [currentNode root]
           (if (= n (:value @currentNode))
             (println "Ignore silently, as duplicates are not allowed")
-            (do
               (cond (< n (:value @currentNode))
-                (do
-;                  (println "Left")
                   (if (nil? (:left @currentNode))
                     (swap! currentNode assoc :left (atom (->Node n nil nil)))
                     (recur (:left @currentNode))
                     )
-                )
                 (> n (:value @currentNode))
-                (do
-;                  (println "Right")
                   (if (nil? (:right @currentNode))
                     (swap! currentNode assoc :right (atom (->Node n nil nil)))
                     (recur (:right @currentNode))
                     )
                 )
-                )
-              )
             )
           
           )
-        )
       )
-;    (println "--------------Added a new Node")
   )
 
 (defn printElements
@@ -50,7 +34,7 @@
     (if (:left @node)
       (printElements (:left @node))
       )
-    (println (:value @node))
+    (print (:value @node) "  ")
     (if (:right @node)
       (printElements (:right @node))
       )    
