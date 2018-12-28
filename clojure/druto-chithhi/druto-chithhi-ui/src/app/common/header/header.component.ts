dc952477358b5c61e8b1dd619b6680260ca1608f
Palash Ray <paawak@gmail.com>
2018-12-28 06:49:29 +0530

    added custom header

---
+++ clojure/druto-chithhi/druto-chithhi-ui/src/app/common/header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-druto-chithhi-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent {
+
+}
