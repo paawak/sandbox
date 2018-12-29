cefcb3b243a956cefb56b9b79661c77eeb47d329
Palash Ray <paawak@gmail.com>
2018-12-29 07:58:14 +0530

    added header styles

--- clojure/druto-chithhi/druto-chithhi-ui/src/app/common/header/header.component.ts
+++ clojure/druto-chithhi/druto-chithhi-ui/src/app/common/header/header.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-druto-chithhi-header',
-  templateUrl: './header.component.html'
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
 
