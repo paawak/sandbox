1f3e0e633024f2ff54048e483eb6644722748024
Palash Ray <paawak@gmail.com>
2019-01-02 08:36:59 +0530

    made constant static, final

--- clojure/druto-chithhi/druto-chithhi-ui/src/app/country/country.service.ts
+++ clojure/druto-chithhi/druto-chithhi-ui/src/app/country/country.service.ts
@@ -8,12 +8,12 @@ import { Country } from './country.model';
 @Injectable()
 export class CountryService {
 
-      private COUNTRY_URL_PREFIX = 'http://192.168.1.4:8080/country';
+      private static readonly COUNTRY_URL_PREFIX = 'http://192.168.1.4:8080/country';
 
       constructor(private httpClient: HttpClient) {}
 
       public getCountries(): Observable<Country[]> {
-        return <Observable<Country[]>>this.httpClient.get(this.COUNTRY_URL_PREFIX);
+        return <Observable<Country[]>>this.httpClient.get(CountryService.COUNTRY_URL_PREFIX);
       }
 
       public getCountry(countryId: number): Observable<Country> {
