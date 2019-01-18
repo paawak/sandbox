99a58013140b2f26876fa215a153ca9035292220
Palash Ray <paawak@gmail.com>
2019-01-18 23:54:28 +0530

    added more tests

--- clojure/druto-chithhi/address-store/src/test/clojure/com/swayam/products/drutochithhi/addrstore/controller/CountryControllerTest.clj
+++ clojure/druto-chithhi/address-store/src/test/clojure/com/swayam/products/drutochithhi/addrstore/controller/CountryControllerTest.clj
@@ -65,8 +65,49 @@
    )
   )
 
+(deftest add-new-country-json-data-test
+  (let [ response (response-for server :post "/country" 
+                     :body "{\"name\":\"Finland\", \"shortname\":\"FN\"}"
+                     :headers {"Content-Type" "application/json"}
+                   )
+         body (:body response)
+         headers (:headers response)
+         contentType (get headers "Content-Type")
+        ]
+    (log/info "printing all headers...")
+    (print-map headers)
+	  (is (=
+	       body
+	       "{\"id\":3}"
+        ))
+	  (is (=
+	       contentType
+	       "application/json;charset=UTF-8"))
+   )
+  )
+
+(deftest add-new-country-bad-data-no-content-type-test
+  (let [ response (response-for server :post "/country" )
+         body (:body response)
+         headers (:headers response)
+         contentType (get headers "Content-Type")
+        ]
+    (log/info "printing all headers...")
+    (print-map headers)
+	  (is (=
+	       body
+	       "{\"error\":\"Bad request: Unsupported media type\"}"
+        ))
+	  (is (=
+	       contentType
+	       "application/json;charset=UTF-8"))
+   )
+  )
+
 ;; run the tests in the correct order
 (defn test-ns-hook []
   (list-countries-test)
   (add-new-country-form-data-test)
+  (add-new-country-json-data-test)
+  (add-new-country-bad-data-no-content-type-test)
   )
