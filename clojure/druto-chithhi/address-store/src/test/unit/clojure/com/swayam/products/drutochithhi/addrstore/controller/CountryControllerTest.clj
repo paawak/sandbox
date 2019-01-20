7df4089daffe44d223df537721137b368f3d1577
Palash Ray <paawak@gmail.com>
2019-01-20 23:42:58 +0530

    blank unit test that does nothing

---
+++ clojure/druto-chithhi/address-store/src/test/unit/clojure/com/swayam/products/drutochithhi/addrstore/controller/CountryControllerTest.clj
@@ -0,0 +1,13 @@
+(ns com.swayam.products.drutochithhi.addrstore.controller.CountryControllerTest
+  (:require [clojure.test :refer :all]
+            [io.pedestal.test :refer :all]
+            [io.pedestal.http :as http]
+            [clojure.java.jdbc :as jdbc]
+            [clojure.tools.logging :as log]
+            [com.swayam.products.drutochithhi.addrstore.controller.CountryController :as testClass]
+            ))
+
+
+(deftest country-interceptor-test
+  (log/info "Blank test case for now!")
+  )
