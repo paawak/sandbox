db8c1c4cfbcb2cdd27f25ffec6225ba70f448e91
Palash Ray <paawak@gmail.com>
2019-01-21 21:39:28 +0530

    simple example of function stubbing using *with-redefs-fn*

--- clojure/druto-chithhi/address-store/src/test/unit/clojure/com/swayam/products/drutochithhi/addrstore/controller/CountryControllerTest.clj
+++ clojure/druto-chithhi/address-store/src/test/unit/clojure/com/swayam/products/drutochithhi/addrstore/controller/CountryControllerTest.clj
@@ -3,14 +3,19 @@
             [io.pedestal.test :refer :all]
             [io.pedestal.http :as http]
             [clojure.java.jdbc :as jdbc]
+            [io.pedestal.interceptor.chain :as chain]
             [clojure.tools.logging :as log]
             [com.swayam.products.drutochithhi.addrstore.controller.CountryController :as testClass]
             ))
 
 
-(deftest country-interceptor-test
-  (log/info "Blank test case for now!")
+(deftest country-interceptor-test-no-content-type
   ;;http://clojuredocs.org/clojure.core/binding
   ;;http://clojuredocs.org/clojure.core/with-redefs
   ;;http://clojuredocs.org/clojure.core/with-redefs-fn
+  (let [enterFunction (:enter testClass/country-interceptor)]
+    (with-redefs-fn {#'chain/terminate (fn [context] (log/info "!!!!!!!! STUBBED **chain/terminate**") true)}
+      #(is (= true (enterFunction {})))
+      )
+    )
   )
