0c126caf9f86e1bf5c37dd91b43513bf56fbc5ec
Palash Ray <paawak@gmail.com>
2020-04-04 21:28:07 +0530

    clojure

--- clojure/learn-clojure/src/learnclojure/core.clj
+++ clojure/learn-clojure/src/learnclojure/core.clj
@@ -2,6 +2,27 @@
 (:gen-class)
 )
 
+;(co2-estimate 2009)
+(defn co2-estimate
+  "Estimates CO2"
+  [year]
+  (let [startYear 2006 initialPPM 382 factor 2]
+           (println "calculating co2 for the year " year)
+           (+ initialPPM (* factor (- year startYear)))
+    )
+  )
+
+;(calmness 1 "hello")
+;(calmness 5 "hello")
+;(calmness 10 "hello")
+(defn calmness
+  "degrees of calmness"
+  [calm text]
+  (cond (< calm 5) (str (clojure.string/upper-case text) ", I TELL YA!") 
+  (and (>= calm 5) (<= calm 9))  (clojure.string/capitalize text)
+  (>= calm 10)  (clojure.string/reverse text)
+  )
+  )
 
 (defn findFact
   [d]
