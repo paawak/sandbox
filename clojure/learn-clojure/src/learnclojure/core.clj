aa752aea8b2e985c351a03899b9371034b74b325
Palash Ray <paawak@gmail.com>
2020-04-06 20:41:28 +0530

    a

--- clojure/learn-clojure/src/learnclojure/core.clj
+++ clojure/learn-clojure/src/learnclojure/core.clj
@@ -29,14 +29,52 @@
   "encodes with the best algo"
   [msg]
   (println msg)
+  (let [num (count (char-array msg))]
   (clojure.string/replace msg #"\w"  (fn [letter] (
     let [charArray (char-array letter) 
          firstChar (first charArray) 
          intValue (int firstChar)
-         totalLength (count (char-array msg))
-         seed (+ intValue totalLength)
-         ] (str (Math/pow seed 2))) ))
+         seed (+ intValue num)
+         ] (str "#" (int (Math/pow seed 2)))) ))
   )
+  )
+
+(def gemstone-db {
+    :ruby {
+      :name "Ruby"
+      :stock 120
+      :sales [1990 3644 6376 4918 7882 6747 7495 8573 5097 1712]
+      :properties {
+        :dispersion 0.018
+        :hardness 9.0
+        :refractive-index [1.77 1.78]
+        :color "Red"
+      }
+    }
+   :diamond {
+      :name "Diamond"
+      :stock 10
+      :sales [8295 329 5960 6118 4189 3436 9833 8870 9700 7182 7061 1579]
+      :properties {
+        :dispersion 0.044
+        :hardness 10
+        :refractive-index [2.417 2.419]
+        :color "Typically yellow, brown or gray to colorless"
+      }
+    }
+    :moissanite {
+      :name "Moissanite"
+      :stock 45
+      :sales [7761 3220]
+      :properties {
+        :dispersion 0.104
+        :hardness 9.5
+        :refractive-index [2.65 2.69]
+        :color "Colorless, green, yellow"
+      }
+    }
+  }
+)
 
 (defn findFact
   [d]
