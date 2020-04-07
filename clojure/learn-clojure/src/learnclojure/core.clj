faf4cb0cfe7f997645d09634fb002f983a3b7965
Palash Ray <paawak@gmail.com>
2020-04-08 00:58:04 +0530

    reading file

--- clojure/learn-clojure/src/learnclojure/core.clj
+++ clojure/learn-clojure/src/learnclojure/core.clj
@@ -1,4 +1,5 @@
 (ns learnclojure.core
+  (:require [clojure.java.io :as io] )
 (:gen-class)
 )
 
@@ -75,6 +76,86 @@
     }
   }
 )
+
+
+(def game-users
+  [{:id 9342
+    :username "speedy"
+    :current-points 45
+    :remaining-lives 2
+    :experience-level 5
+    :status :active}
+   {:id 9854
+    :username "stealthy"
+    :current-points 1201
+    :remaining-lives 1
+    :experience-level 8
+    :status :speed-boost}
+   {:id 3014
+    :username "sneaky"
+    :current-points 725
+    :remaining-lives 7
+    :experience-level 3
+    :status :active}
+   {:id 2051
+    :username "forgetful"
+    :current-points 89
+    :remaining-lives 4
+    :experience-level 5
+    :status :imprisoned}
+   {:id 1032
+    :username "wandering"
+    :current-points 2043
+    :remaining-lives 12
+    :experience-level 7
+    :status :speed-boost}
+   {:id 7213
+    :username "slowish"
+    :current-points 143
+    :remaining-lives 0
+    :experience-level 1
+    :status :speed-boost}
+   {:id 5633
+    :username "smarter"
+    :current-points 99
+    :remaining-lives 4
+    :experience-level 4
+    :status :terminated}
+   {:id 3954
+    :username "crafty"
+    :current-points 21
+    :remaining-lives 2
+    :experience-level 8
+    :status :active}
+   {:id 7213
+    :username "smarty"
+    :current-points 290
+    :remaining-lives 5
+    :experience-level 12
+    :status :terminated}
+   {:id 3002
+    :username "clever"
+    :current-points 681
+    :remaining-lives 1
+    :experience-level 8
+    :status :active}])
+
+;(game-stats game-users)
+(defn game-stats
+  [players]
+  (map #(:current-points %) players
+    ) 
+  )
+
+;(tennis-stats "match_scores_1877-1967_unindexed_csv.csv")
+(defn tennis-stats
+  [statsFileCsv]
+  (let [dataFile (io/resource statsFileCsv)
+        rawLines (slurp dataFile)
+        ]
+    (println (count rawLines))
+    ) 
+  )
 
 (defn findFact
   [d]
